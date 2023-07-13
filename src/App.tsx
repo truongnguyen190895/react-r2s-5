5cc4264867ce6d9aecc82c48a6fa70cdf79f2bb8
truong <ntruong@switch.tv>
2023-07-13 19:34:10 +0700

    complete form

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,67 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./App.css";
-import { Item } from "./components/Item";
-import Button from 'react-bootstrap/Button';
 
-interface Phone {
-  id: string;
-  model: string;
-  brand: string;
-}
-
-const PHONES: Phone[] = [
-  { id: "abc", model: "iphone 14", brand: "apple" }, // 0 => 0
-  { id: "ss1", model: "Samsung Note 10", brand: "samsung" },
-  { id: "abb", model: "iphone 14 Pro Max", brand: "apple" }, // 1 => lost
-  { id: "ss", model: "Samsung Note 10", brand: "samsung" },
-  { id: "abv", model: "iphone 14 Mini", brand: "apple" }, // 2 => 1
-  { id: "ss2", model: "Samsung S22", brand: "samsung" },
-];
+const validationSchema = Yup.object().shape({
+  job: Yup.string()
+    .min(2, "Too Short!")
+    .max(20, "Too Long!")
+    .required("Job can not be empty!"),
+  email: Yup.string().email("Is not email form, please check again").required("Email is required!"),
+});
 
 function App() {
-  const [brand, setBrand] = useState<string>("all");
-  const [phones, setPhones] = useState<Phone[]>(PHONES);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      job: "",
+    },
+    onSubmit: (values) => {
+      console.log("form value from formik ", values);
+    },
+    onReset: () => {
+      console.log("reset form");
+    },
+    validationSchema: validationSchema,
+  });
 
-  const onClickBrand = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    if (event.target.value === "all") {
-      setPhones(PHONES);
-    } else {
-      setPhones(PHONES.filter((phone) => phone.brand === event.target.value));
-    }
-    setBrand(event.target.value);
-  };
+  // console.log("formik ", formik);
 
   return (
-    <div style={{ padding: "10px" }}>
-      <div>Filter by brand</div>
-      <label>
-        All
+    <div className="App">
+      <h1>Simple Form demo</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address*</label>
         <input
-          type="radio"
-          name="brand"
-          value="all"
-          checked={brand === "all"}
-          onChange={onClickBrand}
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
         />
-      </label>
-      <br />
-      <label>
-        Apple
-        <input
-          type="radio"
-          name="brand"
-          value="apple"
-          checked={brand === "apple"}
-          onChange={onClickBrand}
-        />
-      </label>
-      <br />
-      <label>
-        Samsung
-        <input
-          type="radio"
-          name="brand"
-          value="samsung"
-          checked={brand === "samsung"}
-          onChange={onClickBrand}
-        />
-      </label>
-      <br />
-      <label>
-        NOKIA
-        <input
-          type="radio"
-          name="brand"
-          value="nokia"
-          checked={brand === "nokia"}
-          onChange={onClickBrand}
-        />
-      </label>
-      <br />
+        <br />
+        {formik.errors.email && formik.touched.email ? (
+          <small style={{ color: "red" }}>{formik.errors.email}</small>
+        ) : null}
 
-      <Button variant="primary" size="sm">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-
-      {phones.length > 0 ? (
-        phones.map((phone) => <Item key={phone.id} name={phone.model} />)
-      ) : (
-        <h1>No phone found</h1>
-      )}
+        <br />
+        <label htmlFor="email">Job title*</label>
+        <input
+          id="job"
+          type="text"
+          name="job"
+          value={formik.values.job}
+          onChange={formik.handleChange}
+        />
+        <br />
+        {formik.errors.job && formik.touched.job ? (
+          <small style={{ color: "red" }}>{formik.errors.job}</small>
+        ) : null}
+        <br />
+        <button type="submit">Click to submit</button>
+        <button onClick={formik.handleReset}>Click to reset</button>
+      </form>
     </div>
   );
 }
